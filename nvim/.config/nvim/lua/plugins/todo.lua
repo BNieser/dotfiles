f42f59a17d19db6f63947b903c5ae314f75bec4b
Björn Nieser <bjoern.nieser@t-online.de>
2025-12-29 22:11:46 +0100

    feat add removeme to todo.lua

--- nvim/.config/nvim/lua/plugins/todo.lua
+++ nvim/.config/nvim/lua/plugins/todo.lua
@@ -1,3 +1,13 @@
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' },
+	opts = { signs = false,
+		keywords = {
+			REMOVEME = {
+			        icon = " ",
+          			color = "error",
+          			alt = { "REMOVE", "DELETE" },
+		}, }, }, },
 }
+
+
+
