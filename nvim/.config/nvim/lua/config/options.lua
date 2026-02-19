f1f0027e11fd0a0513239e21b1dde9d8a2489d76
Björn Nieser <bjoern.nieser@t-online.de>
2026-02-19 23:12:59 +0100

    refactor(nvim): change tab settings

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+
+-- change global tab settings
+vim.opt.expandtab = true -- exchange the tab symbol with spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
