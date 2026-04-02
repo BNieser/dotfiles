2c0897f621c0bca06bed6aa64027527b424c8fe7
Björn Nieser <bjoern.nieser@t-online.de>
2026-04-02 17:04:16 +0200

    feat(nvim): add colorcolum

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,7 @@ require("config.lazy") -- TODO: move also to core.lazy
 require("core.lsp")
 require("config.keymaps")
 require("config.autocmds")
+
+-- Needs to be set after the colosheme was loaded or set it directly
+-- in the colosheme
+vim.cmd("highlight ColorColumn ctermbg=0 guibg=#05dce3")
