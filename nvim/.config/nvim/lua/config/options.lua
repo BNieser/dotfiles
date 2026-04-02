a9e9736b4998a6df29f974ce3e2a25cc0a04294d
Björn Nieser <bjoern.nieser@t-online.de>
2026-04-02 17:06:19 +0200

    feat(nvim): update options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,21 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.wrap = false
+
+-- Column bei 80 anzeigen
+vim.opt.colorcolumn = "80,88"
 
 -- change global tab settings
 vim.opt.expandtab = true -- exchange the tab symbol with spaces
+vim.opt.smartindent = true
+vim.opt.autoindent = true -- copy indentation from current line
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+
+-- search options
+vim.opt.ignorecase = true -- allow for ignoreing  case sensitive search when using lower case
+vim.opt.smartcase = true -- use case  sensitive serach when a uppercase is in the string
+vim.opt.hlsearch = true -- highlights search matches
+vim.opt.incsearch = true -- allow for incementalseraches
