147919ed4cd08f92383face4cb17d7c35574bb93
Bjoern Nieser <bjoern.nieser@t-online.de>
2026-07-14 19:08:21 +0200

    feat(hypr): allow enabling loggin

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -4,3 +4,14 @@ require("lua.looks")
 require("lua.env")
 require("lua.input")
 require("lua.keybind")
+require("lua.workspaces")
+
+-- for seeign prinitgn we can enable the logs
+-- they can be viewed with
+-- hyprctl rollinglog -f | grep Lua
+hl.config({
+	debug = {
+		-- enable_stdout_logs = true,
+		disable_logs = true,
+	},
+})
